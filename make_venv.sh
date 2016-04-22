fcc0dacb3f4c8dff5493e7c75b12311c10fa7575
sal <slpg500a>
2016-04-22 23:52:04 +0100

    Can build graphite

--- make_venv.sh
+++ make_venv.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+workspace="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 virtualenv venv --python=`which python2.7` --prompt="(ladygeek)" --clear --verbose
 source venv/bin/activate
-venv/bin/pip install -r requirements.txt
+graphite_dir=${workspace}/graphite
+venv/bin/pip install -r requirements.txt --install-option="--prefix=${graphite_dir}/graphite" --install-option="--install-lib=${graphite_dir}/graphite/lib"
